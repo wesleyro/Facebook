06247526e6d1d700b0a0e323673409749a0bc889
Wesley <lele.robert18@gmail.com>
2022-12-12 14:40:52 -0300

    coloquei caixa de login branca com sombra

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
 
-import { Info, Container,Column,Column1 } from './styles'
+import { Info, Container,Column,Menu } from './styles'
 const Home=()=>{
     return(
         <>
 
         <Container>
-            
 
-        <Column>
- 
-        <Info>
-                <img src={logo} alt='logo'/>
-                <h2>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</h2>
-            </Info>
-        </Column>
-        <Column1>
- 
-        <Info>
-                <img src={logo} alt='logo'/>
-                <h2>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</h2>
-            </Info>
-        </Column1>
+            <Column>
 
-       
+                <Info>
+                    {/*esta logo e o texto do facebook*/}
+                    <img src={logo} alt='logo'/>
+                    <h2>O Facebook ajuda você a se conectar e compartilhar com as pessoas que </h2><h2>fazem parte da sua vida.</h2>
+                </Info>
+
+            </Column>
+
+            <Menu>
+                {/*aqui esta a caixa de login */}
+
+            </Menu>
+                
         </Container>
             
         </>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,47 +3,49 @@ import ('https://fonts.googleapis.com/css2?family=Amethysta&family=Open+Sans:wgh
 export const Container = styled.main`
     width: 100%;
     display:flex;
-
-
+    margin-left:11.5%;
+    margin-right:10%;
+  
 
 
 `
+
 export const Column= styled.main`
-    width: 100%;
     display:flex;
-    margin-left:157px;
     margin-top:176px;
-    padding-right:200px;
+    width: 610px;
 `
 
 
 export const Info = styled.div`
 
     img{
-
+ 
         width:300px;
 
     }
     h2{
-
         font-family: 'Open Sans', sans-serif;
-        width: 100%;
+        width: 600px;
         font-size:26px;
         padding-left:35px;
 
-        padding-right:300px;
         font-weight:500;
-        
-        
     
     }
 
 
 `
-export const Column1= styled.main`
-    width: 100%;
-    max-width: 80%;
-    display:flex;
-    margin-top:176px;
-    padding-right:200px;
+
+export const Menu=styled.div`
+margin-top:130px;
+
+display:flex;
+width: 395px;
+height:350px;
+background-color:#FFFFFF;
+border-radius: 10px;
+z-index:1;
+box-shadow: 0 0 5px rgba(0,0,0,0.2);
+-webkit-box-shadow: 0 0 5px rgba(0,0,0,0.2);
 `
